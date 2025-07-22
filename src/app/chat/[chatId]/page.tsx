61ce8067567b96f3b63386d02e7d7cd5ea21d71d
tiredtiredness <tiredtiredness@gmail.com>
2025-07-22 20:53:18 +0300

    fix: changed title on chat page

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -3,7 +3,7 @@ import {ChatPage} from '@/components/pages/ChatPage';
 
 export async function generateMetadata({params}: {params: {chatId: string}}): Promise<Metadata> {
   return {
-    title: `Чат с ${params.chatId} | Impersonator Chat`,
+    title: `Чат с имперсонатором | Impersonator Chat`,
     description:
       'Наслаждайся диалогом с AI, воссоздающим стиль и мышление выбранной личности. Увлекательные беседы с Эйнштейном, Маском и другими.',
   };
